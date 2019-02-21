f88aebd447ad37b5ec366ebfdbc160605eb52b0a
priyanka216668 <priyanka@joshsoftware.com>
2019-02-21 22:55:44 +0530

    Defined TeamMemberService in dependencies.do

--- server/dependencies.go
+++ server/dependencies.go
@@ -3,11 +3,13 @@ package server
 import (
 	"github.com/A9u/function_junction/app"
 	"github.com/A9u/function_junction/category"
+	"github.com/A9u/function_junction/team_member"
 	"github.com/A9u/function_junction/db"
 )
 
 type dependencies struct {
 	CategoryService category.Service
+	TeamMemberService team_member.Service
 }
 
 func initDependencies() (dependencies, error) {
@@ -16,8 +18,10 @@ func initDependencies() (dependencies, error) {
 	dbStore := db.NewStorer(appDB)
 
 	categoryService := category.NewService(dbStore, logger, app.GetCollection("catagories"))
+	teamMemberService := team_member.NewService(dbStore, logger, app.GetCollection("team_members"))
 
 	return dependencies{
 		CategoryService: categoryService,
+		TeamMemberService: teamMemberService,
 	}, nil
 }
