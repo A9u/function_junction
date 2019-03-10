f26fa310257fe50649f831b435e3baa8a97edd8a
Shweta kale <shweta@joshsoftware.com>
2019-03-10 12:11:03 +0530

    User details in event

--- db/user.go
+++ db/user.go
@@ -2,7 +2,7 @@ package db
 
 import (
 	"context"
-
+  "fmt"
 	"github.com/A9u/function_junction/app"
 	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/bson/primitive"
@@ -11,13 +11,21 @@ import (
 
 type User struct {
 	ID    primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Name  string             `json:"name"`
+  FirstName  string         `bson:"first_name" json:"public_profile"`
+	LastName  string             `json:"last_name"`
+  PublicProfile publicProfile         `json: "public_profile" bson:"public_profile"`
 	Email string             `json:"email"`
 }
 
+type publicProfile struct {
+  FirstName string `json:"first_name"`
+  LastName string  `json:"last_name"`
+}
 func FindUserByID(ctx context.Context, userID primitive.ObjectID) (userDetails User, err error) {
 	collection := app.GetCollection("users")
 	err = collection.FindOne(ctx, bson.D{{"_id", userID}}).Decode(&userDetails)
+  userDetails.FirstName = "test"
+  userDetails.LastName = "test"
 	return userDetails, err
 }
 
