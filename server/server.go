4c8e70b672205c4f376acd217eb603e1dc3dee94
Anusha Bhat <anusha@joshsoftware.com>
2021-03-09 21:46:56 +0530

    Add cors and headers

--- server/server.go
+++ server/server.go
@@ -5,13 +5,16 @@ import (
 	"strconv"
 
 	"github.com/A9u/function_junction/config"
+	"github.com/rs/cors"
 	"github.com/urfave/negroni"
 )
 
 func StartAPIServer() {
 	port := config.AppPort()
 
+	c := cors.New(cors.Options{Debug: true, AllowedHeaders: []string{"authorization", "content-type"}, AllowedMethods: []string{"GET", "POST", "DELETE"}})
 	server := negroni.Classic()
+	server.Use(c)
 	dependencies, err := initDependencies()
 	server.Use(negroni.HandlerFunc(AuthMiddleware))
 	if err != nil {
